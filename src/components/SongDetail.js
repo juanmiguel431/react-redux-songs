3bd543d2a5ed2848963f578c0f2a9e4a5cf462b5
juanmiguel431 <juanmiguel431@gmail.com>
2022-10-02 18:06:51 -0400

    Song Detail - Song duration added.

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -4,8 +4,12 @@ import { connect } from "react-redux";
 const SongDetail = ({ song }) => {
   return (
     <div className="song-detail">
-      <h1>Song detail</h1>
-      <h2>{song?.title}</h2>
+      <h3>Details for</h3>
+      <p>
+        Title: {song?.title || 'Select a song'}
+        <br/>
+        duration: {song?.duration}
+      </p>
     </div>
   );
 };
